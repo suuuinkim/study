7a796ea9c9619f1e866c954ee5a24e1c0927efe2
sikim <sikim@dataeum.kr>
2025-07-08 20:41:13 +0900

    feat : 백준#2309 일곱난장이

--- src/main/java/Practice01.java
+++ src/main/java/Practice01.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Parctice01 {
+public class Practice01 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
